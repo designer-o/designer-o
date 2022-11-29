22f32c8f541fe9c3bdc406d32bd87ffbfe767270
Sirswa <crazy4designes@gmail.com>
2022-11-29 21:42:17 +0530

    added redirect link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,15 @@
-import type {
-  NextPage,
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-} from "next";
-import Head from "next/head";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Work from "../components/Work";
-import Skills from "../components/Skills";
-import Cta from "../components/Cta";
+import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-import { Cursor } from "react-creative-cursor";
-import "react-creative-cursor/dist/styles.css";
+const Home: NextPage = () => {
+  const router = useRouter();
 
-const Home: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ data }) => {
-  return (
-    <div className="container mx-auto px-5 sm:px-10">
-      <Cursor isGelly={true} cursorSize={20} />
-      <Head>
-        <title>designer_o | figma expert ui ux designer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <Hero />
-      </div>
-      <About />
-      <Skills />
-      <Work data={data} />
-      <Cta />
-    </div>
-  );
+  useEffect(() => {
+    router.push("https://iamharish.vercel.app/");
+  }, []);
+
+  return <div>redirecting...</div>;
 };
 
 export default Home;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch(
-    `https://api.dribbble.com/v2/user/shots?access_token=0cd8ca4207a6d5bd13ca0eed5e84578f04db37abaf60a37b7ad4b73b4a76502b`
-  );
-  const data = await res.json();
-
-  // Pass data to the page via props
-  return { props: { data } };
-};
